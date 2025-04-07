76ee8ccb88ff98ced8c21cefda845d03c161c61c
zorrrrrro <yashkant1125@gmail.com>
2025-04-07 13:10:48 +0530

    Remove GitHub Pages config and revert to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import github from '@astrojs/github-pages';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  base: '/portfolio/',
+  output: 'static',
+  integrations: [github()],
 });
